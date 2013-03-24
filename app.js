06c6b465f95ec9c1a436001fcc2ef1418cbcb027
Michael Yong <wrong92@gmail.com>
2013-03-24 23:36:44 +0800

    increase twss threshold

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ var say = function(message) {
 //
 // twss
 //
-twss.threshold = 0.7;
+twss.threshold = 0.9;
 bot.addListener('message', function(from, to, message) {
   if (twss.is(message)) {
     say("that's what she said.");
